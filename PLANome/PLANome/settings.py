932f5f813b39d882d1b50882719c9023c3c84472
Mr.Geo22 <karami.mohsen@protonmail.com>
2019-07-07 18:31:25 +0430

    fix a secrity issue!

--- PLANome/PLANome/settings.py
+++ PLANome/PLANome/settings.py
@@ -141,8 +141,9 @@ CART_SESSION_ID = 'cart'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'info.planome@gmail.com'
-EMAIL_HOST_PASSWORD = 'tnwrkhhdesorulge'
+EMAIL_HOST_PASSWORD = '**************'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
