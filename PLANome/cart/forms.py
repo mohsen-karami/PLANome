b95d4f6f9ccfe1a210c98994eae066c986eee6ee
Mr.Geo22 <karami.mohsen@protonmail.com>
2019-07-17 16:19:39 +0430

    remove 'تغییر تعداد' button and the form will submit automatically after changing the quantity of dropdown list

--- PLANome/cart/forms.py
+++ PLANome/cart/forms.py
@@ -5,5 +5,5 @@ PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
 
 
 class CartAddProductForm(forms.Form):
-    quantity = forms.TypedChoiceField(label=False, choices=PRODUCT_QUANTITY_CHOICES, coerce=int)
+    quantity = forms.TypedChoiceField(label=False, choices=PRODUCT_QUANTITY_CHOICES, coerce=int, widget = forms.Select(attrs = {'onchange' : "form.submit();"}))
     update = forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput)
