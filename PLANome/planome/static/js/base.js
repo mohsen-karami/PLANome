18eadda5b107fa35f2aee7c8d8c9ac6c18d85603
Mr.Geo22 <karami.mohsen@protonmail.com>
2019-07-24 22:30:33 +0430

    convert english numbers to arabic numbers

--- PLANome/planome/static/js/base.js
+++ PLANome/planome/static/js/base.js
@@ -1,3 +1,8 @@
+var replaceDigits = function() {
+  var map = ["&\#1776;","&\#1777;","&\#1778;","&\#1779;","&\#1780;", "&\#1781;","&\#1782;","&\#1783;","&\#1784;","&\#1785;"]
+  document.body.innerHTML = document.body.innerHTML.replace(/\d(?=[^<>]*(<|$))/g, function($0) { return map[$0]});
+}
+
 function detect_device() {
     if (innerWidth <= innerHeight) {
         document.getElementById('landscape').setAttribute('href', 'static/css/base-landscape-mobile.css');
@@ -6,4 +11,5 @@ function detect_device() {
     }
 }
 
+window.onload = replaceDigits;
 detect_device();
