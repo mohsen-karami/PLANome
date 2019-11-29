4fa38969ed97d34e8152ad15d511dc7b06446361
Mr.Geo22 <karami.mohsen@protonmail.com>
2019-11-29 23:50:55 +0330

    Fix an issue of porting python 2 code to 3

--- PLANome/PLANome/settings.py
+++ PLANome/PLANome/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'orders',
 ]
 
-MIDDLEWARE_CLASSES = [
+MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
