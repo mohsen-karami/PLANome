4cd7dc74d456111d2b45b8631c90fd5a06e6a99f
Mr.Geo22 <karami.mohsen@protonmail.com>
2020-02-02 22:58:50 +0330

    Fix 'object is not JSON serializable' issue

--- PLANome/PLANome/settings.py
+++ PLANome/PLANome/settings.py
@@ -145,3 +145,4 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+SESSION_SERIALIZER = 'django.contrib.sessions.serializers.PickleSerializer'
