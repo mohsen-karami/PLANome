a689850eafdb2866d01904e002a55c9d574724ac
Mr.Geo22 <karami.mohsen@protonmail.com>
2020-03-06 22:41:12 +0330

    Add verification to customer login/register when place an order

--- PLANome/orders/views.py
+++ PLANome/orders/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from .models import OrderItem, Order
 from planome.models import Customer
 from .forms import OrderCreateForm, OrderAuthenticateForm
+from django.contrib.auth.hashers import make_password, check_password
 from cart.cart import Cart
 
 
@@ -13,18 +14,32 @@ def order_authenticate(request):
         if form.is_valid():
             customer = Customer.objects.filter(phone_number=request.POST['phone_number'])
             if customer:
-                order = Order.objects.create(customer=customer[0])
-                for item in cart:
-                    OrderItem.objects.create(order=order, product=item['product'], price=item['price'], quantity=item['quantity'])
-                cart.clear()
-                return render(request, 'orders/order/created.html', {'order': order, 'customer': customer[0]})
+                if check_password(form.cleaned_data['password'], customer[0].password):
+                    order = Order.objects.create(customer=customer[0])
+                    for item in cart:
+                        OrderItem.objects.create(order=order, product=item['product'], price=item['price'], quantity=item['quantity'])
+                    cart.clear()
+                    request.session['customer'] = customer[0]
+                    request.session.save()
+                    return render(request, 'orders/order/created.html', {'order': order, 'customer': customer[0]})
+                else:
+                    form = OrderAuthenticateForm()
+                    return render(request, 'orders/order/authenticate.html', {'cart': cart, 'form': form})
             else:
                 form = OrderCreateForm()
                 return render(request, 'orders/order/create.html', {'form': form, 'phone_number': request.POST['phone_number']})
         else:
             form = OrderAuthenticateForm()
     else:
-        form = OrderAuthenticateForm()
+        if 'customer' in request.session:
+            customer = request.session['customer']
+            order = Order.objects.create(customer=customer)
+            for item in cart:
+                OrderItem.objects.create(order=order, product=item['product'], price=item['price'], quantity=item['quantity'])
+            cart.clear()
+            return render(request, 'orders/order/created.html', {'order': order, 'customer': customer})
+        else:
+            form = OrderAuthenticateForm()
     return render(request, 'orders/order/authenticate.html', {'cart': cart, 'form': form})
 
 
@@ -33,12 +48,38 @@ def order_create(request):
     if request.method == 'POST':
         form = OrderCreateForm(request.POST)
         if form.is_valid():
-            customer = form.save()
-            order = Order.objects.create(customer=customer)
-            for item in cart:
-                OrderItem.objects.create(order=order, product=item['product'], price=item['price'], quantity=item['quantity'])
-            cart.clear()
-            return render(request, 'orders/order/created.html', {'order': order, 'customer': customer})
+            customer = Customer.objects.filter(phone_number=request.POST['phone_number'])
+            if customer:
+                formPassword = make_password(form.cleaned_data['password'])
+                if formPassword == customer[0].password:
+                    order = Order.objects.create(customer=customer)
+                    for item in cart:
+                        OrderItem.objects.create(order=order, product=item['product'], price=item['price'], quantity=item['quantity'])
+                    cart.clear()
+                    request.session['customer'] = customer[0]
+                    request.session.save()
+                    return render(request, 'orders/order/created.html', {'order': order, 'customer': customer})
+                else:
+                    form = OrderAuthenticateForm()
+                    return render(request, 'orders/order/create.html', {'form': form})
+            else:
+                if form.cleaned_data['password'] == form.cleaned_data['rePassword']:
+                    formPassword = make_password(form.cleaned_data['password'])
+                    customer = form.save(commit=False)
+                    customer.password = formPassword
+                    customer.save()
+                    request.session['customer'] = customer
+                    request.session.save()
+                    order = Order.objects.create(customer=customer)
+                    for item in cart:
+                        OrderItem.objects.create(order=order, product=item['product'], price=item['price'], quantity=item['quantity'])
+                    cart.clear()
+                    return render(request, 'orders/order/created.html', {'order': order, 'customer': customer})
+                else:
+                    form = OrderCreateForm(request.POST)
+                    return render(request, 'orders/order/create.html', {'form': form})
+        else:
+            form = OrderCreateForm()
     else:
         form = OrderCreateForm()
     return render(request, 'orders/order/create.html', {'form': form})
