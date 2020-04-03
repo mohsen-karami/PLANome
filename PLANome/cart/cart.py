6064fcb80b56cee6afaf49b49d7ab205dea34389
Mr.Geo22 <karami.mohsen@protonmail.com>
2020-04-03 20:18:13 +0430

    Calculate off prices in shopping card

--- PLANome/cart/cart.py
+++ PLANome/cart/cart.py
@@ -14,7 +14,7 @@ class Cart(object):
     def add(self, product, quantity=1, update_quantity=False):
         product_id = str(product.id)
         if product_id not in self.cart:
-            self.cart[product_id] = {'quantity': 0, 'price': str(product.price)}
+            self.cart[product_id] = {'quantity': 0, 'price': str(product.price), 'off_price': str(product.off_price)}
         if update_quantity:
             self.cart[product_id]['quantity'] = quantity
         else:
@@ -40,6 +40,9 @@ class Cart(object):
         for item in self.cart.values():
             item['price'] = Decimal(item['price'])
             item['total_price'] = item['price'] * item['quantity']
+            if item.get('off_price'):
+                item['off_price'] = Decimal(item['off_price'])
+                item['total_off_price'] = item['off_price'] * item['quantity']
             yield item
 
     def __len__(self):
@@ -47,6 +50,18 @@ class Cart(object):
 
     def get_total_price(self):
         return sum(Decimal(item['price']) * item['quantity'] for item in self.cart.values())
+
+    def get_total_off_price(self):
+        total_off_price = 0
+        for item in self.cart.values():
+            if item.get('off_price'):
+                total_off_price += (Decimal(item['price']) - Decimal(item['off_price'])) * item['quantity']
+        return total_off_price
+
+    def get_total_final_price(self):
+        total_price = self.get_total_price()
+        total_off_price = self.get_total_off_price()
+        return total_price - total_off_price
 
     def clear(self):
         self.session[settings.CART_SESSION_ID] = {}
