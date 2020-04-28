3a6c6d1a8f03b0124d8e0fcb4d1abaa0fc6ab5c5
Mr.Geo22 <karami.mohsen@protonmail.com>
2020-04-28 21:58:36 +0430

    Detach cart function of the product view page and the cart details page

--- PLANome/cart/urls.py
+++ PLANome/cart/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.cart_detail, name='cart_detail'),
     url(r'^add/(?P<product_id>\d+)/$', views.cart_add, name='cart_add'),
+    url(r'^change/(?P<product_id>\d+)/$', views.cart_change, name='cart_change'),
     url(r'^remove/(?P<product_id>\d+)/$', views.cart_remove, name='cart_remove')
 ]
--- PLANome/cart/views.py
+++ PLANome/cart/views.py
@@ -16,6 +16,15 @@ def cart_add(request, product_id):
         cart.add(product=product, quantity=cd['quantity'], update_quantity=cd['update'])
     return redirect('planome:product_detail', id=product_id, slug=product.slug)
 
+def cart_change(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    form = CartAddProductForm(request.POST)
+    if form.is_valid():
+        cd = form.cleaned_data
+        cart.add(product=product, quantity=cd['quantity'], update_quantity=cd['update'])
+    return redirect('cart:cart_detail')
+
 def cart_remove(request, product_id):
         cart = Cart(request)
         product = get_object_or_404(Product, id=product_id)
